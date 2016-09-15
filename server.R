b3dd0399eef3ac2ce43792dbecf531e418e2f48d
Nicholas Clark <clarkno@mail.uc.edu>
2016-09-15 12:24:38 -0400

    boxplot fix

--- server.R
+++ server.R
@@ -31,22 +31,140 @@ shinyServer(function(input,output,session) {
     }
     boxplot_data = full_data[full_data[[ input$pick_box_x ]] %in% input$pick_box_factors,]
     boxplot_data = boxplot_data[is.finite(boxplot_data[[parameter_choice]]),]
+    boxplot_data[[ input$pick_box_x ]] = factor(boxplot_data[[ input$pick_box_x ]])
+    
+    if(!is.null(input$factorB) & !is.null(input$factorA)) {
+      for(i in 1:length(input$factorB)) {
+        boxplot_data[[ input$pick_box_x ]] = relevel(boxplot_data[[ input$pick_box_x ]], input$factorB[i])
+      }
+      for(i in 1:length(input$factorA)) {
+        boxplot_data[[ input$pick_box_x ]] = relevel(boxplot_data[[ input$pick_box_x ]], input$factorA[i])
+      }
+    }
+    
     x_factor = factor(get(input$pick_box_x, envir = as.environment(boxplot_data)))
     y_variable = get(parameter_choice, envir = as.environment(boxplot_data))
     point_color = factor(get(input$pick_box_point_color, envir = as.environment(boxplot_data)))
     if(dim(boxplot_data)[1] > 0) {
       p <- ggplot(boxplot_data, aes(x = x_factor, y = y_variable))
-      p = p + geom_boxplot(aes(fill = x_factor, alpha = 0.3), outlier.color = NA, show.legend = F) + geom_jitter(width = 0.5, show.legend = F, aes(colour = point_color)) + xlab('') + ylab(parameter_choice) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-      plotScatter_box <<- p
+      p = p + geom_boxplot(aes(fill = x_factor, alpha = 0.3), outlier.color = NA, show.legend = F) + geom_jitter(width = 0.5, show.legend = F, aes(colour = point_color)) + xlab('') + ylab(parameter_choice)
+      
       # modify x and y names for hovertext
       #test_gg <<- plotly_build(p)
-      p = plotly_build(p)
-      for(i in 1:length(p$data)){
-        p$data[[i]]$text = gsub('x_factor', input$pick_box_x, p$data[[i]]$text)
-        p$data[[i]]$text = gsub('y_variable', parameter_choice, p$data[[i]]$text)
+      #test_gg<<- q
+      p1 = plotly_build(p)
+      #test_box <<- p1
+      # Get y range:
+      top_y = p1[[2]]$yaxis$range[2]
+      bottom_y = p1[[2]]$yaxis$range[1]
+      total_y_range = top_y - bottom_y
+      
+      q <- ggplot(boxplot_data, aes(x = x_factor, y = y_variable, ymin = bottom_y, ymax = top_y))
+      q = q + geom_boxplot(aes(fill = x_factor, alpha = 0.3), outlier.color = NA, show.legend = F) + geom_jitter(width = 0.5, aes(colour = point_color)) + xlab('') + ylab(parameter_choice) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+      q$labels$colour = input$pick_box_point_color
+      
+      if(!is.null(values$wilcox)) {
+        # Get top of boxplot whiskers
+        whiskers = NULL
+        #for(i in 1:length(levels(x_factor))) {
+        len = length(input$factorA) + length(input$factorB)
+        for(i in 1:len) {
+          whiskers[i] = fivenum(p1[[1]][[i]]$y)[5]
+        }
+        top_whisker = max(whiskers, na.rm = TRUE)
+        y_range = (top_y - top_whisker)/total_y_range
+        if(y_range < .25) {
+          top_y = top_whisker + .25*total_y_range
+          #y_range = top_y - top_whisker
+        }
+        lh = top_whisker + total_y_range*(.1)
+        bump = total_y_range*(.05)
+        ll = lh - bump
+        lenA = length(input$factorA)
+        lenB = length(input$factorB)
+        
+        q <- ggplot(boxplot_data, aes(x = x_factor, y = y_variable, ymin = bottom_y, ymax = top_y))
+        q = q + geom_boxplot(aes(fill = x_factor, alpha = 0.3), outlier.color = NA, show.legend = F) + geom_jitter(width = 0.5, aes(colour = point_color)) + xlab('') + ylab(parameter_choice) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+        q$labels$colour = input$pick_box_point_color
+        
+        if(lenA == 1 & lenB == 1) {
+          p = p + annotate("text", x = 1.5, y = lh + bump/2, label = paste("p =",values$wilcox)) + geom_segment(x = 1, y = lh, xend = 2, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh)
+          
+          q = q + annotate("text", x = 1.5, y = lh + bump/2, label = paste("p =",values$wilcox)) + geom_segment(x = 1, y = lh, xend = 2, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+          
+        } else if(lenA > 1 & lenB == 1) {
+          p = p + annotate("text", x = ((lenA + 1) + ((lenA+1)/2))/2, y = lh + 2*bump, label = paste("p =",values$wilcox)) +
+            geom_segment(x = 1, y = lh, xend = lenA, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = lenA, y = ll, xend = lenA, yend = lh) +
+            geom_segment(x = (lenA+1)/2, y = lh + bump, xend = lenA + 1, yend = lh + bump) + geom_segment(x = (lenA+1)/2, y = lh, xend = (lenA+1)/2, yend = lh + bump) + geom_segment(x = lenA+1, y = ll, xend = lenA+1, yend = lh + bump)
+          
+          q = q + annotate("text", x = ((lenA + 1) + ((lenA+1)/2))/2, y = lh + 2*bump, label = paste("p =",values$wilcox)) +
+            geom_segment(x = 1, y = lh, xend = lenA, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = lenA, y = ll, xend = lenA, yend = lh) +
+            geom_segment(x = (lenA+1)/2, y = lh + bump, xend = lenA + 1, yend = lh + bump) + geom_segment(x = (lenA+1)/2, y = lh, xend = (lenA+1)/2, yend = lh + bump) + geom_segment(x = lenA+1, y = ll, xend = lenA+1, yend = lh + bump) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+        } else if(lenA == 1 & lenB > 1) {
+          p = p + annotate("text", x = 1.25 + .25*lenB, y = lh + 2*bump, label = paste("p =",values$wilcox)) + 
+            geom_segment(x = 1, y = lh+bump, xend = .5*lenB + 1.5, yend = lh+bump) + geom_segment(x = 1, y = ll, xend = 1, yend = lh+bump) + geom_segment(x = 1.5+.5*lenB, y = lh, xend = 1.5+.5*lenB, yend = lh+bump) +
+            geom_segment(x = 2, y = lh, xend = lenB + 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh) + geom_segment(x = lenB+1, y = ll, xend = lenB+1, yend = lh)
+          
+          q = q + annotate("text", x = 1.25 + .25*lenB, y = lh + 2*bump, label = paste("p =",values$wilcox)) + 
+            geom_segment(x = 1, y = lh+bump, xend = .5*lenB + 1.5, yend = lh+bump) + geom_segment(x = 1, y = ll, xend = 1, yend = lh+bump) + geom_segment(x = 1.5+.5*lenB, y = lh, xend = 1.5+.5*lenB, yend = lh+bump) +
+            geom_segment(x = 2, y = lh, xend = lenB + 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh) + geom_segment(x = lenB+1, y = ll, xend = lenB+1, yend = lh) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+        } else if(lenA > 1 & lenB > 1) {
+          p = p + annotate("text", x = .25*(lenB-1)+.75*(lenA+1), y = lh + 2*bump, label = paste("p =",values$wilcox)) + 
+            geom_segment(x = 1, y = lh, xend = lenA, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = lenA, y = ll, xend = lenA, yend = lh) +
+            geom_segment(x = lenA+1, y = lh, xend = lenA+lenB, yend = lh) + geom_segment(x = lenA+1, y = ll, xend = lenA+1, yend = lh) + geom_segment(x = lenA+lenB, y = ll, xend = lenA+lenB, yend = lh) +
+            geom_segment(x = (lenA+1)/2, y = lh+bump, xend = (lenA+1)+((lenB-1)/2), yend = lh+bump) + geom_segment(x = (lenA+1)/2, y = lh, xend = (lenA+1)/2, yend = lh+bump) + geom_segment(x = (lenA+1)+((lenB-1)/2), y = lh, xend = (lenA+1)+((lenB-1)/2), yend = lh+bump)
+          
+          q = q + annotate("text", x = .25*(lenB-1)+.75*(lenA+1), y = lh + 2*bump, label = paste("p =",values$wilcox)) + 
+            geom_segment(x = 1, y = lh, xend = lenA, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = lenA, y = ll, xend = lenA, yend = lh) +
+            geom_segment(x = lenA+1, y = lh, xend = lenA+lenB, yend = lh) + geom_segment(x = lenA+1, y = ll, xend = lenA+1, yend = lh) + geom_segment(x = lenA+lenB, y = ll, xend = lenA+lenB, yend = lh) +
+            geom_segment(x = (lenA+1)/2, y = lh+bump, xend = (lenA+1)+((lenB-1)/2), yend = lh+bump) + geom_segment(x = (lenA+1)/2, y = lh, xend = (lenA+1)/2, yend = lh+bump) + geom_segment(x = (lenA+1)+((lenB-1)/2), y = lh, xend = (lenA+1)+((lenB-1)/2), yend = lh+bump) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) # + theme_grey(base_size = 14) 
+        }
+        
+        p = plotly_build(p)
+        p[[2]]$yaxis$range[2] = top_y
+      } else {
+        p = plotly_build(p)
       }
-      p$layout$xaxis$tickangle = -90
-      p$layout$margin$b = 200
+      plotScatter_box <<- q
+      
+      # Current CRAN version of plotly (3.6.0) uses p$data
+      # Latest github version of plotly (4.3.5) uses p$x$data
+      if(is.null(p$data)) {
+        for(i in 1:length(p$x$data)) {
+          if(!is.null(p$x$data[[i]]$text)) {
+            p$x$data[[i]]$text = gsub('x_factor', input$pick_box_x, p$x$data[[i]]$text)
+            p$x$data[[i]]$text = gsub('y_variable', parameter_choice, p$x$data[[i]]$text)
+          }
+        }
+        # p$layout$xaxis$tickangle = -90
+        # p$layout$margin$b = 200
+        bottom_margin = max(nchar(p$layout$xaxis$ticktext), na.rm = TRUE)
+        left = nchar(p$layout$xaxis$ticktext[1])
+        p$layout$xaxis$tickangle = -45
+        p$layout$margin$b = 15 + 6*bottom_margin
+        if(left > 10) {
+          left_margin = p$layout$margin$l + (left-10)*6
+          p$layout$margin$l = left_margin
+        }
+      } else {
+        for(i in 1:length(p$data)){
+          if(!is.null(p$data[[i]]$text)) {
+            p$data[[i]]$text = gsub('x_factor', input$pick_box_x, p$data[[i]]$text)
+            p$data[[i]]$text = gsub('y_variable', parameter_choice, p$data[[i]]$text)
+          }
+        }
+        # p$layout$xaxis$tickangle = -90
+        # p$layout$margin$b = 200
+        bottom_margin = max(nchar(p$layout$xaxis$ticktext), na.rm = TRUE)
+        left = nchar(p$layout$xaxis$ticktext[1])
+        p$layout$xaxis$tickangle = -45
+        p$layout$margin$b = 15 + 6*bottom_margin
+        if(left > 10) {
+          left_margin = p$layout$margin$l + (left-10)*6
+          p$layout$margin$l = left_margin
+        }
+      }
+      test_box <<- p
       return(p)
     }
   }
@@ -131,7 +249,7 @@ shinyServer(function(input,output,session) {
     if(input$drg_demo != '') {
       q = drawExamplePopup(input, values)
       output$graphPopupPlotDemo <- renderPlotly({
-        try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+        #try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
         ggplotly(q)
         layout(
           xaxis = list(range = c(-4,2),
@@ -149,7 +267,7 @@ shinyServer(function(input,output,session) {
     q = parseLabel(input, values, full_data)
     if (input$'dose-response-grid-main' != '') {
       output$graphPopupPlot <- renderPlotly({
-        try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+        #try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
         ggplotly(q)
         layout(
           xaxis = list(range = c(-4,2),
@@ -205,7 +323,7 @@ observeEvent(input$plot_scatter, {
   output$plotlyScatter1 <- renderPlotly({
     plot1 = isolate(drawScatter(input, values))
 print(1.1)    
-try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+#try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
     ggplotly(plot1)
     layout(plot1, 
            margin = list(
@@ -266,21 +384,62 @@ observeEvent(input$browseDataset, {
 })
 
 observeEvent(input$pick_box_x, {
-  factor_choices = sort(unique(full_data[[input$pick_box_x]]))
+  picks = sort(unique(full_data[[input$pick_box_x]]))
   updateSelectizeInput(
     session, 'pick_box_factors',
-    choices = factor_choices,
-    selected = factor_choices[1]
+    choices = picks,
+    selected = picks[1:min(10, length(picks))]
   )
 })
 
 observeEvent(input$box_scatter, {
   if(!is.null(input$pick_box_x)) {
-    factor_choices = sort(unique(full_data[[input$pick_box_x]]))
+    picks = sort(unique(full_data[[input$pick_box_x]]))
     updateSelectizeInput(
       session, 'pick_box_factors',
-      choices = factor_choices,
-      selected = factor_choices[1]
+      choices = picks,
+      selected = picks[1:min(10, length(picks))]
+    )
+  }
+})
+
+observeEvent(input$factorA, {
+  picks = sort(input$pick_box_factors)
+  picks1 = setdiff(picks, input$factorA)
+  updateSelectizeInput(
+    session, 'factorB',
+    choices = picks1,
+    selected = input$factorB
+  )
+})
+
+observeEvent(input$factorB, {
+  picks = sort(input$pick_box_factors)
+  picks2 = setdiff(picks, input$factorB)
+  updateSelectizeInput(
+    session, 'factorA',
+    choices = picks2,
+    selected = input$factorA
+  )
+})
+
+observeEvent(c(input$box_scatter, input$pick_box_x, input$pick_box_factors), {
+  if(!is.null(input$pick_box_x)) {
+    print(input$pick_box_factors)
+    print(typeof(input$pick_box_factors))
+    print(class(input$pick_box_factors))
+    picks = sort(input$pick_box_factors)
+    updateSelectizeInput(
+      session, 'factorA',
+      #choices = unique(values$GR_table[[input$pick_box_x]]),
+      choices = picks,
+      selected = NULL
+    )
+    updateSelectizeInput(
+      session, 'factorB',
+      #choices = unique(values$GR_table[[input$pick_box_x]]),
+      choices = picks,
+      selected = NULL
     )
   }
 })
@@ -314,7 +473,7 @@ observeEvent(input$pick_var, {
 
 observeEvent(input$browseDataset, {
   output$boxplot <- renderPlotly({
-    try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+    #try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
     box = redrawPlotlyBox(input, values)
     if(!is.null(box)) {
       box
@@ -345,8 +504,36 @@ output$scatter <- renderUI({
       selectInput('pick_box_y', 'Select parameter', choices = c('GR50', 'GRmax', 'GRinf', 'Hill', 'GR_AOC')),
       selectInput('pick_box_x', 'Select grouping variable', choices = values$config$groupableColumns),
       selectInput('pick_box_point_color', 'Select additional point coloring', choices = values$config$groupableColumns),
-      selectizeInput('pick_box_factors', 'Select factors of grouping variable', choices = c(), multiple = T)
+      selectizeInput('pick_box_factors', 'Select factors of grouping variable', choices = c(), multiple = T),
+      selectizeInput('factorA', 'Choose factors with which to perform a one-sided Wilcoxon rank-sum test', choices = c(), multiple = T),
+      selectizeInput('factorB', '', choices = c(), multiple = T),
+      textOutput("wilcox")
     )
+  }
+})
+
+observeEvent(c(input$factorA, input$factorB, input$pick_box_y), {
+  wil_data = full_data
+  if(!is.null(input$factorA) & !is.null(input$factorB)) {
+    print(input$pick_box_x)
+    rowsA = wil_data[[input$pick_box_x]] %in% input$factorA
+    rowsB = wil_data[[input$pick_box_x]] %in% input$factorB
+    print(rowsA)
+    print(rowsB)
+    wil_dataA = wil_data[rowsA,input$pick_box_y]
+    wil_dataB = wil_data[rowsB,input$pick_box_y]
+    print(head(wil_dataA))
+    print(head(wil_dataB))
+    wil = wilcox.test(x = wil_dataA, y = wil_dataB, alternative = "less")
+    values$wilcox = prettyNum(wil$p.value, digits = 2)
+    output$wilcox = renderText({
+      paste("P-value:", prettyNum(wil$p.value, digits = 2))
+    })
+  } else {
+    output$wilcox = renderText({
+      paste("P-value: ")
+    })
+    values$wilcox = NULL
   }
 })
 
@@ -378,7 +565,7 @@ observeEvent(input$browseDataset, {
     
     df_full <<- NULL
     print(3)
-    try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+    #try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
     ggplotly(p)
     print(4)
     layout(p, hovermode = FALSE)
@@ -412,7 +599,7 @@ observeEvent(input$clear, {
 
     df_full <<- NULL
 print(3)
-try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
+#try(png(paste("/mnt/raid/tmp/junk1",gsub(" ","_",date()),as.character(as.integer(1000000*runif(1))),".png",sep="_")))
     ggplotly(p)
 print(4)
     layout(p, hovermode = FALSE)
@@ -420,8 +607,8 @@ print(4)
   })
   
 })
-  cancel.onSessionEnded <- session$onSessionEnded(function() {
-    graphics.off()
-    print('devices off')
-  })
+  # cancel.onSessionEnded <- session$onSessionEnded(function() {
+  #   graphics.off()
+  #   print('devices off')
+  # })
 })
